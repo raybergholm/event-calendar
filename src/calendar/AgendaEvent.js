08b71336ee24170abcb054043069f9952f4c0372
Ray Bergholm <raybergholm@gmail.com>
2018-01-25 16:13:19 +0200

    add null checks

--- src/calendar/AgendaEvent.js
+++ src/calendar/AgendaEvent.js
@@ -3,7 +3,7 @@ import React from "react";
 const AgendaEvent = ({event}) => (
     <span>
         <em>{event.title}</em>
-        <p>{`${event.place.name}${event.place.location && event.place.location.street && event.place.location.city ? `${event.place.location.street}, ${event.place.location.city}`: null}`}</p><br />
+        <p>{event.place && event.place.name ? `${event.place.name}${event.place.location && event.place.location.street && event.place.location.city ? `${event.place.location.street}, ${event.place.location.city}`: null}` : null}</p>
         <p>{event.description}</p>
     </span>
 );
