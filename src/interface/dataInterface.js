bb696f39b0154f9c26f761aa84e2e7b1cdf33036
Ray Bergholm <raybergholm@gmail.com>
2018-01-31 11:56:54 +0200

    implement course schedule -> events array parser

--- src/interface/dataInterface.js
+++ src/interface/dataInterface.js
@@ -43,28 +43,45 @@ function importData() {
 }
 
 function generateSchedule(schools) {
-    return null;
+    return buildEventsList(schools);
 }
 
 function buildEventsList(schools) {
-    schools.reduce((reducer, school) => {
-        let startDate = moment(school.season.start);
-        let endDate = moment(school.season.end);
+    let events = schools.reduce((reducer, school) => {
+        let seasonStart = moment(school.season.start);
+        let seasonEnd = moment(school.season.end);
 
         school.season.courses.forEach((course) => {
-            for(let date = startDate.clone(); date < endDate; date = date.add(7, "days")){
-                let event = {};
+            for(let date = seasonStart.clone(); date < seasonEnd; date = date.clone().add(7, "days")){ // date.add(7, "days") works too since it's a mutator, but let's make it obvious that there's an assignment
                 for(let day in course.schedule){
                     let eventStart = date.clone().day(day);
                     let eventEnd = eventStart.add(course.schedule[day].duration, "minutes");
+
+                    reducer.push({
+                        title: course.name,
+                        level: course.level,
+                        location: course.location,
+                        teacher: course.teacher,
+                        start: eventStart.toDate(),
+                        end: eventEnd.toDate()
+                    });
                 }
-                
-                reducer.push(event);
             }
         });
-
         return reducer;
     }, []);
+
+    events.sort((left, right) => {
+        if(!left.start || !right.start || left.start.getTime() === right.start.getTime()){
+            return 0;
+        }else if(left.start.getTime() < right.start.getTime()){
+            return -1;
+        }else {
+            return 1;
+        }
+    });
+
+    return events;
 }
 
 export default dataInterface;
