c3a82341db35f1dc3a3becb2f72269a94e923cf7
Ray Bergholm <raybergholm@gmail.com>
2018-02-01 16:44:38 +0200

    add dateTimeUtils tests

--- src/test/Templates.test.js
+++ src/test/Templates.test.js
@@ -6,21 +6,54 @@ import Schedule from "../template/Schedule";
 import School from "../template/School";
 
 describe("Template tests", () => {
-    describe("Course", () => {
+    describe("Schedule", () => {
+        const emptySchedule = {
+            start: null,
+            end: null,
+            days: {},
+            except: [],
+        };
         it("No input data should return null", () => {
-            expect(new Course()).to.equal(null);
+            expect(new Schedule()).to.be.null;
+        });
+
+        it("Empty object input returns a Schedule object with null properties", () => {
+            expect(new Course({})).to.equal(emptySchedule);
         });
     });
 
-    describe("Schedule", () => {
+    describe("Course", () => {
+        const emptyCourse = {
+            name: null,
+            level: null,
+            tags: null,
+            location: null,
+            teacher: null,
+            url: null,
+            schedule: null
+        };
         it("No input data should return null", () => {
-            expect(new Schedule()).to.equal(null);
+            expect(new Course()).to.be.null;
+        });
+
+        it("Empty object input returns a Course object with null properties", () => {
+            expect(new Course({})).to.equal(emptyCourse);
         });
     });
 
     describe("School", () => {
+        const emptySchool = {
+            schoolName: null,
+            websiteUrl: null,
+            location: null,
+            courses: null
+        };
         it("No input data should return null", () => {
-            expect(new School()).to.equal(null);
+            expect(new School()).to.be.null;
+        });
+
+        it("Empty object input returns a School object with null properties", () => {
+            expect(new School({})).to.equal(emptySchool);
         });
     });
 });
---
+++ src/test/dateTimeUtils.test.js
@@ -0,0 +1,27 @@
+import {
+    describe,
+    it
+} from "mocha";
+
+import {
+    expect
+} from "chai";
+
+import {
+    startOfDay,
+    endOfDay,
+    correctTimezoneOffset,
+    buildTimestamp
+} from "../utils/dateTimeUtils";
+
+import moment from "moment";
+
+describe("dateTimeUtils tests", () => {
+    it("buildTimestamp", () => {
+
+    });
+
+    describe("", () => {
+        it();
+    });
+});
